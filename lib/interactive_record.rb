6778080196872f1f7da0d6efcf6b24a42a472e9a
ryan m <contact@ryanmeek.com>
2020-01-29 01:57:27 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -38,7 +38,7 @@ class InteractiveRecord
   end
 
   def self.find_by(query)
-    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE ? = ?", query.keys[0], "'#{query.values[0]}'")
+    DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE ? = ?", query.keys[0], "#{query.values[0]}")
   end
 
 end
